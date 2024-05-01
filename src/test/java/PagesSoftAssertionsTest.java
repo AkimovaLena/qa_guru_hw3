02601237f1ee6c4e2a94f7a4b01732e87a7e7296
Elena Akimova <kulikovaelena19@yandex.ru>
2024-05-01 14:21:48 +0300

    hw3 v2

--- src/test/java/PagesSoftAssertionsTest.java
+++ src/test/java/PagesSoftAssertionsTest.java
@@ -1,8 +1,6 @@
-import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -21,16 +19,24 @@ public class PagesSoftAssertionsTest {
         // Перейдите в раздел Wiki проекта
         $("#wiki-tab").click();
         // Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
-        $$(".markdown-body ul li").find(text("Soft assertions")).shouldBe(visible);
-
         // Откройте страницу SoftAssertions
-        $(byText("Soft assertions")).click();
-        $("div.gh-header").$("h1").shouldHave(text("SoftAssertions")).click();
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $("#wiki-pages-box").find(byText("SoftAssertions")).click();
         //проверьте что внутри есть пример кода для JUnit5
-        $$("div.markdown-body div.markdown-heading").findBy(text("JUnit5")).sibling(0).shouldHave(text("class Tests"));
-        //!!! Изначально написала  с parent() (строчка ниже) и долго не могла понять почему не работает, опытным путем добилась результата выше, но нет понимания почему так работает, а так нет..
-        //$$("div.markdown-body div.markdown-heading").findBy(text("JUnit5")).parent().sibling(0).shouldHave(text("class Tests"));
-
+        $$("div.markdown-body div.markdown-heading").findBy(text("JUnit5")).sibling(0)
+                .shouldHave(text("""
+                        @ExtendWith({SoftAssertsExtension.class})
+                        class Tests {
+                          @Test
+                          void test() {
+                            Configuration.assertionMode = SOFT;
+                            open("page.html");
+                                                
+                            $("#first").should(visible).click();
+                            $("#second").should(visible).click();
+                          }
+                        }
+                        """));
     }
 }
 
